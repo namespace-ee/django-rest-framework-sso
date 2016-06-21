679f02dfc66fa2e71644e671266800a99b71b972
Lenno Nagel <lenno@nagel.ee>
2016-06-21 13:22:07 +0300

    Add packaging information

---
+++ README.rst
@@ -0,0 +1,16 @@
+=====
+Django REST Framework SSO
+=====
+
+Django REST Framework SSO is an extension to Django REST Framework that enables
+Single sign-on in a microservice-oriented environment using the JWT standard.
+
+Quick start
+-----------
+
+1. Add "rest_framework_sso" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'rest_framework_sso',
+    ]
---
+++ setup.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='djangorestframework-sso',
+    version='0.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Single sign-on extension to the Django REST Framework.',
+    long_description=README,
+    url='https://github.com/namespace-ee/django-rest-framework-sso',
+    author='Lenno Nagel',
+    author_email='lenno@namespace.ee',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'PyJWT>=1.4.0,<2.0.0',
+    ],
+)
