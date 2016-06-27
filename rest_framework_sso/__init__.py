8786c6abd4a9e72741f4a2461abcf862d6cc0533
Lenno Nagel <lenno@nagel.ee>
2016-06-27 11:46:06 +0300

    Version 0.0.3

--- rest_framework_sso/__init__.py
+++ rest_framework_sso/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 __title__ = 'djangorestframework-sso'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Lenno Nagel'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Namespace OÜ'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangorestframework-sso',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
