bb220940339d8331f50bde5b3429968ee8e9eb89
Lenno Nagel <lenno@nagel.ee>
2018-05-17 17:10:04 +0300

    Fix requirements conflicts with cryptography

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangorestframework-sso',
-    version='0.0.9',
+    version='0.0.10',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='MIT License',
