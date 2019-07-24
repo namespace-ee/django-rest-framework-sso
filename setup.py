8265ae978c91bab1471920b1467d75d9ebcc3242
Lenno Nagel <lenno@nagel.ee>
2019-07-24 11:46:19 +0300

    0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="djangorestframework-sso",
-    version="0.0.10",
+    version="0.1.0",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     license="MIT License",
@@ -25,10 +25,9 @@ setup(
     author="Lenno Nagel",
     author_email="lenno@namespace.ee",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 1.9",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -38,6 +37,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Session",
         "Topic :: Software Development :: Libraries :: Python Modules",
